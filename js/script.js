e19fa0f4fc7a68e02ed052c44604fc7f983c8cba
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-18 02:21:30 +0000

    setting up

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("npm grunt");
+console.log('npm-grunt'); // to check if script is working
+
+$(document).ready(function(){
+
+  //to check if jquery node_module is working
+  $('body').click(function(){
+    $(this).css('background','cyan');
+  });
+});
+
+$.getJSON('http://api.unsplash.com/users/brianhaferkamp/photos/?client_id=a2b1c2dfb235cdf6342519d09311b85c0a2bf52b2460f3cd7ebca041a44e59f5',
+function(data){
+  console.log(data);
+
+  $.each(data, function(index, value) {
+    console.log(value);
+
+    var name = value.user.name;
+    var bio = value.user.bio;
+    var imageURL = value.urls.regular;
+    console.log(imageURL);
+  });
+});
